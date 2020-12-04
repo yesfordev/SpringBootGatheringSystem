6b193971ec7a3d2f0725300cf51b4c3835b95782
yesfordev <yesfordev@gmail.com>
2020-12-04 11:24:37 +0900

    [UPDATE] uri name, querystring name 수정

--- src/main/java/com/wefunding/wdh/gs/ecos/controller/FrontTestController.java
+++ src/main/java/com/wefunding/wdh/gs/ecos/controller/FrontTestController.java
@@ -13,33 +13,33 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("fronttest")
+@RequestMapping("front")
 @RequiredArgsConstructor
 public class FrontTestController {
 
     private final SearchEntityRepository searchEntityRepository;
 
 
-    @GetMapping("test")
-    public ResponseEntity<?> getTest(@RequestParam(value = "msg") int masterId, @RequestParam(value = "msg2") int startTime, @RequestParam(value = "msg3") int endTime) {
+    @GetMapping("/statisticSearch")
+    public ResponseEntity<?> getTest(@RequestParam(value = "masterId") int masterId, @RequestParam(value = "startTime") int startTime, @RequestParam(value = "endTime") int endTime) {
         List<SearchEntity> searchEntityList = searchEntityRepository.findtest(masterId,startTime,endTime);
         return new ResponseEntity<>(searchEntityList, HttpStatus.OK);
     }
 
-    @GetMapping("test1")
-    public ResponseEntity<?> getTest1(@RequestParam(value = "msg") int masterId, @RequestParam(value = "msg2") String itemCode1, @RequestParam(value = "msg3") int startTime, @RequestParam(value = "msg4") int endTime) {
+    @GetMapping("/statisticSearch/one")
+    public ResponseEntity<?> getTest1(@RequestParam(value = "masterId") int masterId, @RequestParam(value = "itemCode1") String itemCode1, @RequestParam(value = "startTime") int startTime, @RequestParam(value = "endTime") int endTime) {
         List<SearchEntity> searchEntityList = searchEntityRepository.findtest1(masterId,itemCode1,startTime,endTime);
         return new ResponseEntity<>(searchEntityList, HttpStatus.OK);
     }
 
-    @GetMapping("test2")
-    public ResponseEntity<?> getTest2(@RequestParam(value = "msg") int masterId, @RequestParam(value = "msg2") String itemCode1, @RequestParam(value = "msg3") String itemCode2, @RequestParam(value = "msg4") int startTime, @RequestParam(value = "msg5") int endTime) {
+    @GetMapping("/statisticSearch/two")
+    public ResponseEntity<?> getTest2(@RequestParam(value = "masterId") int masterId, @RequestParam(value = "itemCode1") String itemCode1, @RequestParam(value = "itemCode2") String itemCode2, @RequestParam(value = "startTime") int startTime, @RequestParam(value = "endTime") int endTime) {
         List<SearchEntity> searchEntityList = searchEntityRepository.findtest2(masterId,itemCode1,itemCode2,startTime,endTime);
         return new ResponseEntity<>(searchEntityList, HttpStatus.OK);
     }
 
-    @GetMapping("test3")
-    public ResponseEntity<?> getTest3(@RequestParam(value = "msg") int masterId, @RequestParam(value = "msg2") String itemCode1, @RequestParam(value = "msg3") String itemCode2, @RequestParam(value = "msg4") String itemCode3, @RequestParam(value = "msg5") int startTime, @RequestParam(value = "msg6") int endTime) {
+    @GetMapping("/statisticSearch/three")
+    public ResponseEntity<?> getTest3(@RequestParam(value = "masterId") int masterId, @RequestParam(value = "itemCode1") String itemCode1, @RequestParam(value = "itemCode2") String itemCode2, @RequestParam(value = "itemCode3") String itemCode3, @RequestParam(value = "startTime") int startTime, @RequestParam(value = "endTime") int endTime) {
         List<SearchEntity> searchEntityList = searchEntityRepository.findtest3(masterId,itemCode1,itemCode2,itemCode3,startTime,endTime);
         return new ResponseEntity<>(searchEntityList, HttpStatus.OK);
     }
