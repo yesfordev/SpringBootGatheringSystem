ea1b0ceff974b52cf11fe7c28d4a7f09958b553b
wefunding <byun@wefundingui-MacBookPro.local>
2020-12-16 15:04:48 +0900

    csv CP949

--- src/main/java/com/wefunding/wdh/gs/ecos/controller/SearchDataController.java
+++ src/main/java/com/wefunding/wdh/gs/ecos/controller/SearchDataController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
 import java.util.Optional;
 
 @RestController
@@ -24,7 +25,7 @@ public class SearchDataController {
     SearchDataService searchDataService;
 
     @GetMapping("/export-csv")
-    public ResponseEntity exportCSV(HttpServletResponse response, @RequestParam(value = "masterId") Optional<Integer> masterId, @RequestParam(value = "itemCode1") Optional<String> itemCode1, @RequestParam(value = "itemCode2") Optional<String> itemCode2, @RequestParam(value = "itemCode3") Optional<String> itemCode3, @RequestParam(value = "startTime") Optional<Integer> startTime, @RequestParam(value = "endTime") Optional<Integer> endTime){
+    public ResponseEntity exportCSV(HttpServletResponse response, @RequestParam(value = "masterId") Optional<Integer> masterId, @RequestParam(value = "itemCode1") Optional<String> itemCode1, @RequestParam(value = "itemCode2") Optional<String> itemCode2, @RequestParam(value = "itemCode3") Optional<String> itemCode3, @RequestParam(value = "startTime") Optional<Integer> startTime, @RequestParam(value = "endTime") Optional<Integer> endTime) throws UnsupportedEncodingException {
 
         String filename = "SearchData.csv";
         response.setContentType("text/csv");
@@ -34,47 +35,47 @@ public class SearchDataController {
 
         if(itemCode1.isEmpty()&itemCode2.isEmpty()&itemCode3.isEmpty())
         {
-            return new ResponseEntity(searchDataService.searchTableSaveDataCSV(masterId, itemCode1, itemCode2, itemCode3, startTime, endTime), HttpStatus.OK);
+            return new ResponseEntity(searchDataService.searchTableSaveDataCSV(masterId, startTime, endTime).getBytes("CP949"), HttpStatus.OK);
         }
         else if(itemCode1.isPresent()&itemCode2.isEmpty()&itemCode3.isEmpty())
         {
-            return new ResponseEntity(searchDataService.itemCode1SaveDataCSV(masterId, itemCode1, itemCode2, itemCode3, startTime, endTime), HttpStatus.OK);
+            return new ResponseEntity(searchDataService.itemCode1SaveDataCSV(masterId, itemCode1, startTime, endTime).getBytes("CP949"), HttpStatus.OK);
         }
         else if(itemCode1.isPresent()&itemCode2.isPresent()&itemCode3.isEmpty())
         {
-            return new ResponseEntity(searchDataService.itemCode2SaveDataCSV(masterId, itemCode1, itemCode2, itemCode3, startTime, endTime), HttpStatus.OK);
+            return new ResponseEntity(searchDataService.itemCode2SaveDataCSV(masterId, itemCode1, itemCode2, startTime, endTime).getBytes("CP949"), HttpStatus.OK);
         }
         else if(itemCode1.isPresent()&itemCode2.isPresent()&itemCode3.isPresent())
         {
-            return new ResponseEntity(searchDataService.itemCode3SaveDataCSV(masterId, itemCode1, itemCode2, itemCode3, startTime, endTime), HttpStatus.OK);
+            return new ResponseEntity(searchDataService.itemCode3SaveDataCSV(masterId, itemCode1, itemCode2, itemCode3, startTime, endTime).getBytes("CP949"), HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
-    @GetMapping("/export-json")
-    public ResponseEntity exportJSON(HttpServletResponse response, @RequestParam(value = "masterId") Optional<Integer> masterId, @RequestParam(value = "itemCode1") Optional<String> itemCode1, @RequestParam(value = "itemCode2") Optional<String> itemCode2, @RequestParam(value = "itemCode3") Optional<String> itemCode3, @RequestParam(value = "startTime") Optional<Integer> startTime, @RequestParam(value = "endTime") Optional<Integer> endTime){
-
-        String filename = "SearchData.json";
-        response.setContentType("text/json");
-        String headerKey = "Content-Disposition";
-        String headerValue = String.format("attachment; filename=\"%s\"", filename);
-        response.setHeader(headerKey, headerValue);
-
-        if(itemCode1.isEmpty()&itemCode2.isEmpty()&itemCode3.isEmpty())
-        {
-            return new ResponseEntity(searchDataService.searchTableSaveDataJSON(masterId, itemCode1, itemCode2, itemCode3, startTime, endTime), HttpStatus.OK);
-        }
-        else if(itemCode1.isPresent()&itemCode2.isEmpty()&itemCode3.isEmpty())
-        {
-            return new ResponseEntity(searchDataService.itemCode1SaveDataJSON(masterId, itemCode1, itemCode2, itemCode3, startTime, endTime), HttpStatus.OK);
-        }
-        else if(itemCode1.isPresent()&itemCode2.isPresent()&itemCode3.isEmpty())
-        {
-            return new ResponseEntity(searchDataService.itemCode2SaveDataJSON(masterId, itemCode1, itemCode2, itemCode3, startTime, endTime), HttpStatus.OK);
-        }
-        else if(itemCode1.isPresent()&itemCode2.isPresent()&itemCode3.isPresent())
-        {
-            return new ResponseEntity(searchDataService.itemCode3SaveDataJSON(masterId, itemCode1, itemCode2, itemCode3, startTime, endTime), HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-    }
+//    @GetMapping("/export-json")
+//    public ResponseEntity exportJSON(HttpServletResponse response, @RequestParam(value = "masterId") Optional<Integer> masterId, @RequestParam(value = "itemCode1") Optional<String> itemCode1, @RequestParam(value = "itemCode2") Optional<String> itemCode2, @RequestParam(value = "itemCode3") Optional<String> itemCode3, @RequestParam(value = "startTime") Optional<Integer> startTime, @RequestParam(value = "endTime") Optional<Integer> endTime){
+//
+//        String filename = "SearchData.json";
+//        response.setContentType("text/json");
+//        String headerKey = "Content-Disposition";
+//        String headerValue = String.format("attachment; filename=\"%s\"", filename);
+//        response.setHeader(headerKey, headerValue);
+//
+//        if(itemCode1.isEmpty()&itemCode2.isEmpty()&itemCode3.isEmpty())
+//        {
+//            return new ResponseEntity(searchDataService.searchTableSaveDataJSON(masterId, itemCode1, itemCode2, itemCode3, startTime, endTime), HttpStatus.OK);
+//        }
+//        else if(itemCode1.isPresent()&itemCode2.isEmpty()&itemCode3.isEmpty())
+//        {
+//            return new ResponseEntity(searchDataService.itemCode1SaveDataJSON(masterId, itemCode1, itemCode2, itemCode3, startTime, endTime), HttpStatus.OK);
+//        }
+//        else if(itemCode1.isPresent()&itemCode2.isPresent()&itemCode3.isEmpty())
+//        {
+//            return new ResponseEntity(searchDataService.itemCode2SaveDataJSON(masterId, itemCode1, itemCode2, itemCode3, startTime, endTime), HttpStatus.OK);
+//        }
+//        else if(itemCode1.isPresent()&itemCode2.isPresent()&itemCode3.isPresent())
+//        {
+//            return new ResponseEntity(searchDataService.itemCode3SaveDataJSON(masterId, itemCode1, itemCode2, itemCode3, startTime, endTime), HttpStatus.OK);
+//        }
+//        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//    }
 }
--- src/main/java/com/wefunding/wdh/gs/ecos/service/SearchDataService.java
+++ src/main/java/com/wefunding/wdh/gs/ecos/service/SearchDataService.java
@@ -19,7 +19,7 @@ public class SearchDataService {
     @Autowired
     SearchEntityRepository searchEntityRepository;
 
-    public String searchTableSaveDataCSV(Optional<Integer> masterId, Optional<String> itemCode1, Optional<String> itemCode2, Optional<String> itemCode3, Optional<Integer> startTime, Optional<Integer> endTime) {
+    public String searchTableSaveDataCSV(Optional<Integer> masterId, Optional<Integer> startTime, Optional<Integer> endTime) {
 
         JSONArray jsonArr_result = new JSONArray();
 
@@ -41,7 +41,7 @@ public class SearchDataService {
         String csv = CDL.toString(jsonArr_result);
         return csv;
     }
-    public String itemCode1SaveDataCSV(Optional<Integer> masterId, Optional<String> itemCode1, Optional<String> itemCode2, Optional<String> itemCode3, Optional<Integer> startTime, Optional<Integer> endTime) {
+    public String itemCode1SaveDataCSV(Optional<Integer> masterId, Optional<String> itemCode1, Optional<Integer> startTime, Optional<Integer> endTime) {
 
         JSONArray jsonArr_result = new JSONArray();
 
@@ -63,7 +63,7 @@ public class SearchDataService {
         String csv = CDL.toString(jsonArr_result);
         return csv;
     }
-    public String itemCode2SaveDataCSV(Optional<Integer> masterId, Optional<String> itemCode1, Optional<String> itemCode2, Optional<String> itemCode3, Optional<Integer> startTime, Optional<Integer> endTime) {
+    public String itemCode2SaveDataCSV(Optional<Integer> masterId, Optional<String> itemCode1, Optional<String> itemCode2, Optional<Integer> startTime, Optional<Integer> endTime) {
 
         JSONArray jsonArr_result = new JSONArray();
 
@@ -108,88 +108,88 @@ public class SearchDataService {
         return csv;
     }
 
-    public ResponseEntity searchTableSaveDataJSON(Optional<Integer> masterId, Optional<String> itemCode1, Optional<String> itemCode2, Optional<String> itemCode3, Optional<Integer> startTime, Optional<Integer> endTime) {
-
-        JSONArray jsonArr_result = new JSONArray();
-
-        List<SearchEntity> searchEntityList = searchEntityRepository.searchTableInformation(masterId, startTime, endTime);
-        for (SearchEntity searchEntity : searchEntityList) {
-            JSONObject result = new JSONObject();
-            result.put("statName", searchEntity.getStatName());
-            result.put("ItemCode1", searchEntity.getItemCode1());
-            result.put("ItemName1", searchEntity.getItemName1());
-            result.put("ItemCode2", searchEntity.getItemCode2());
-            result.put("ItemName2", searchEntity.getItemName2());
-            result.put("ItemCode3", searchEntity.getItemCode3());
-            result.put("ItemName3", searchEntity.getItemName3());
-            result.put("UnitName", searchEntity.getUnitName());
-            result.put("Time", searchEntity.getTime());
-            result.put("DataValue", searchEntity.getDataValue());
-            jsonArr_result.put(result);
-        }
-        return new ResponseEntity(jsonArr_result, HttpStatus.OK);
-    }
-    public ResponseEntity itemCode1SaveDataJSON(Optional<Integer> masterId, Optional<String> itemCode1, Optional<String> itemCode2, Optional<String> itemCode3, Optional<Integer> startTime, Optional<Integer> endTime) {
-
-        JSONArray jsonArr_result = new JSONArray();
-
-        List<SearchEntity> searchEntityList = searchEntityRepository.itemCode1Information(masterId,itemCode1, startTime, endTime);
-        for (SearchEntity searchEntity : searchEntityList) {
-            JSONObject result = new JSONObject();
-            result.put("statName", searchEntity.getStatName());
-            result.put("ItemCode1", searchEntity.getItemCode1());
-            result.put("ItemName1", searchEntity.getItemName1());
-            result.put("ItemCode2", searchEntity.getItemCode2());
-            result.put("ItemName2", searchEntity.getItemName2());
-            result.put("ItemCode3", searchEntity.getItemCode3());
-            result.put("ItemName3", searchEntity.getItemName3());
-            result.put("UnitName", searchEntity.getUnitName());
-            result.put("Time", searchEntity.getTime());
-            result.put("DataValue", searchEntity.getDataValue());
-            jsonArr_result.put(result);
-        }
-        return new ResponseEntity(jsonArr_result, HttpStatus.OK);
-    }
-    public ResponseEntity itemCode2SaveDataJSON(Optional<Integer> masterId, Optional<String> itemCode1, Optional<String> itemCode2, Optional<String> itemCode3, Optional<Integer> startTime, Optional<Integer> endTime) {
-
-        JSONArray jsonArr_result = new JSONArray();
-
-        List<SearchEntity> searchEntityList = searchEntityRepository.itemCode2Information(masterId, itemCode1, itemCode2,startTime, endTime);
-        for (SearchEntity searchEntity : searchEntityList) {
-            JSONObject result = new JSONObject();
-            result.put("statName", searchEntity.getStatName());
-            result.put("ItemCode1", searchEntity.getItemCode1());
-            result.put("ItemName1", searchEntity.getItemName1());
-            result.put("ItemCode2", searchEntity.getItemCode2());
-            result.put("ItemName2", searchEntity.getItemName2());
-            result.put("ItemCode3", searchEntity.getItemCode3());
-            result.put("ItemName3", searchEntity.getItemName3());
-            result.put("UnitName", searchEntity.getUnitName());
-            result.put("Time", searchEntity.getTime());
-            result.put("DataValue", searchEntity.getDataValue());
-            jsonArr_result.put(result);
-        }
-        return new ResponseEntity(jsonArr_result, HttpStatus.OK);
-    }
-    public ResponseEntity itemCode3SaveDataJSON(Optional<Integer> masterId, Optional<String> itemCode1, Optional<String> itemCode2, Optional<String> itemCode3, Optional<Integer> startTime, Optional<Integer> endTime) {
-
-        JSONArray jsonArr_result = new JSONArray();
-
-        List<SearchEntity> searchEntityList = searchEntityRepository.itemCode3Information(masterId, itemCode1, itemCode2, itemCode3,startTime, endTime);
-        for (SearchEntity searchEntity : searchEntityList) {
-            JSONObject result = new JSONObject();
-            result.put("statName", searchEntity.getStatName());
-            result.put("ItemCode1", searchEntity.getItemCode1());
-            result.put("ItemName1", searchEntity.getItemName1());
-            result.put("ItemCode2", searchEntity.getItemCode2());
-            result.put("ItemName2", searchEntity.getItemName2());
-            result.put("ItemCode3", searchEntity.getItemCode3());
-            result.put("ItemName3", searchEntity.getItemName3());
-            result.put("UnitName", searchEntity.getUnitName());
-            result.put("Time", searchEntity.getTime());
-            result.put("DataValue", searchEntity.getDataValue());
-            jsonArr_result.put(result);
-        }
-        return new ResponseEntity(jsonArr_result, HttpStatus.OK);
-    }
+//    public ResponseEntity searchTableSaveDataJSON(Optional<Integer> masterId, Optional<String> itemCode1, Optional<String> itemCode2, Optional<String> itemCode3, Optional<Integer> startTime, Optional<Integer> endTime) {
+//
+//        JSONArray jsonArr_result = new JSONArray();
+//
+//        List<SearchEntity> searchEntityList = searchEntityRepository.searchTableInformation(masterId, startTime, endTime);
+//        for (SearchEntity searchEntity : searchEntityList) {
+//            JSONObject result = new JSONObject();
+//            result.put("statName", searchEntity.getStatName());
+//            result.put("ItemCode1", searchEntity.getItemCode1());
+//            result.put("ItemName1", searchEntity.getItemName1());
+//            result.put("ItemCode2", searchEntity.getItemCode2());
+//            result.put("ItemName2", searchEntity.getItemName2());
+//            result.put("ItemCode3", searchEntity.getItemCode3());
+//            result.put("ItemName3", searchEntity.getItemName3());
+//            result.put("UnitName", searchEntity.getUnitName());
+//            result.put("Time", searchEntity.getTime());
+//            result.put("DataValue", searchEntity.getDataValue());
+//            jsonArr_result.put(result);
+//        }
+//        return new ResponseEntity(jsonArr_result, HttpStatus.OK);
+//    }
+//    public ResponseEntity itemCode1SaveDataJSON(Optional<Integer> masterId, Optional<String> itemCode1, Optional<String> itemCode2, Optional<String> itemCode3, Optional<Integer> startTime, Optional<Integer> endTime) {
+//
+//        JSONArray jsonArr_result = new JSONArray();
+//
+//        List<SearchEntity> searchEntityList = searchEntityRepository.itemCode1Information(masterId,itemCode1, startTime, endTime);
+//        for (SearchEntity searchEntity : searchEntityList) {
+//            JSONObject result = new JSONObject();
+//            result.put("statName", searchEntity.getStatName());
+//            result.put("ItemCode1", searchEntity.getItemCode1());
+//            result.put("ItemName1", searchEntity.getItemName1());
+//            result.put("ItemCode2", searchEntity.getItemCode2());
+//            result.put("ItemName2", searchEntity.getItemName2());
+//            result.put("ItemCode3", searchEntity.getItemCode3());
+//            result.put("ItemName3", searchEntity.getItemName3());
+//            result.put("UnitName", searchEntity.getUnitName());
+//            result.put("Time", searchEntity.getTime());
+//            result.put("DataValue", searchEntity.getDataValue());
+//            jsonArr_result.put(result);
+//        }
+//        return new ResponseEntity(jsonArr_result, HttpStatus.OK);
+//    }
+//    public ResponseEntity itemCode2SaveDataJSON(Optional<Integer> masterId, Optional<String> itemCode1, Optional<String> itemCode2, Optional<String> itemCode3, Optional<Integer> startTime, Optional<Integer> endTime) {
+//
+//        JSONArray jsonArr_result = new JSONArray();
+//
+//        List<SearchEntity> searchEntityList = searchEntityRepository.itemCode2Information(masterId, itemCode1, itemCode2,startTime, endTime);
+//        for (SearchEntity searchEntity : searchEntityList) {
+//            JSONObject result = new JSONObject();
+//            result.put("statName", searchEntity.getStatName());
+//            result.put("ItemCode1", searchEntity.getItemCode1());
+//            result.put("ItemName1", searchEntity.getItemName1());
+//            result.put("ItemCode2", searchEntity.getItemCode2());
+//            result.put("ItemName2", searchEntity.getItemName2());
+//            result.put("ItemCode3", searchEntity.getItemCode3());
+//            result.put("ItemName3", searchEntity.getItemName3());
+//            result.put("UnitName", searchEntity.getUnitName());
+//            result.put("Time", searchEntity.getTime());
+//            result.put("DataValue", searchEntity.getDataValue());
+//            jsonArr_result.put(result);
+//        }
+//        return new ResponseEntity(jsonArr_result, HttpStatus.OK);
+//    }
+//    public ResponseEntity itemCode3SaveDataJSON(Optional<Integer> masterId, Optional<String> itemCode1, Optional<String> itemCode2, Optional<String> itemCode3, Optional<Integer> startTime, Optional<Integer> endTime) {
+//
+//        JSONArray jsonArr_result = new JSONArray();
+//
+//        List<SearchEntity> searchEntityList = searchEntityRepository.itemCode3Information(masterId, itemCode1, itemCode2, itemCode3,startTime, endTime);
+//        for (SearchEntity searchEntity : searchEntityList) {
+//            JSONObject result = new JSONObject();
+//            result.put("statName", searchEntity.getStatName());
+//            result.put("ItemCode1", searchEntity.getItemCode1());
+//            result.put("ItemName1", searchEntity.getItemName1());
+//            result.put("ItemCode2", searchEntity.getItemCode2());
+//            result.put("ItemName2", searchEntity.getItemName2());
+//            result.put("ItemCode3", searchEntity.getItemCode3());
+//            result.put("ItemName3", searchEntity.getItemName3());
+//            result.put("UnitName", searchEntity.getUnitName());
+//            result.put("Time", searchEntity.getTime());
+//            result.put("DataValue", searchEntity.getDataValue());
+//            jsonArr_result.put(result);
+//        }
+//        return new ResponseEntity(jsonArr_result, HttpStatus.OK);
+//    }
 }
