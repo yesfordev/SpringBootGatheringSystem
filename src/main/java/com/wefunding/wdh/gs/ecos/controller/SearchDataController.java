72610b91dfc17b3d4f78aeba01cfc12f6d7fec6c
wefunding <byun@wefundingui-MacBookPro.local>
2020-12-17 11:29:50 +0900

    csv조건 수정

--- src/main/java/com/wefunding/wdh/gs/ecos/controller/SearchDataController.java
+++ src/main/java/com/wefunding/wdh/gs/ecos/controller/SearchDataController.java
@@ -2,6 +2,7 @@ package com.wefunding.wdh.gs.ecos.controller;
 
 import com.wefunding.wdh.gs.ecos.repository.SearchEntityRepository;
 import com.wefunding.wdh.gs.ecos.service.SearchDataService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,8 +26,9 @@ public class SearchDataController {
     @Autowired
     SearchDataService searchDataService;
 
+    @ApiOperation(value = "CSV download(CP949 encoding)", notes = "통합")
     @GetMapping("/export-csv")
-    public ResponseEntity exportCSV(HttpServletResponse response, @RequestParam(value = "masterId") Optional<Integer> masterId, @RequestParam(value = "itemName1") Optional<List<String>> itemName1, @RequestParam(value = "startTime") Optional<Integer> startTime, @RequestParam(value = "endTime") Optional<Integer> endTime) throws UnsupportedEncodingException {
+    public ResponseEntity exportCSV(HttpServletResponse response, @RequestParam(value = "masterId") Optional<Integer> masterId, @RequestParam(value = "itemName1") Optional<List<String>> itemName1, @RequestParam(value = "itemCode1") Optional<String> itemCode1, @RequestParam(value = "itemCode2") Optional<String> itemCode2, @RequestParam(value = "itemCode3") Optional<String> itemCode3, @RequestParam(value = "startTime") Optional<Integer> startTime, @RequestParam(value = "endTime") Optional<Integer> endTime) throws UnsupportedEncodingException {
 //, @RequestParam(value = "itemCode2") Optional<String> itemCode2, @RequestParam(value = "itemCode3") Optional<String> itemCode3,
         String filename = "SearchData.csv";
         response.setContentType("text/csv");
@@ -35,10 +37,16 @@ public class SearchDataController {
         response.setHeader(headerKey, headerValue);
 
         try {
-            if (itemName1.isEmpty()) {
+            if (itemName1.isEmpty()&itemCode1.isEmpty()&itemCode2.isEmpty()&itemCode3.isEmpty()) {
                 return new ResponseEntity(searchDataService.searchTableDataCSV(masterId, startTime, endTime).getBytes("CP949"), HttpStatus.OK);
-            } else if (itemName1.isPresent()) {
+            } else if (itemName1.isPresent()&itemCode1.isEmpty()&itemCode2.isEmpty()&itemCode3.isEmpty()) {
                 return new ResponseEntity(searchDataService.multiAreaDataCSV(masterId, itemName1, startTime, endTime).getBytes("CP949"), HttpStatus.OK);
+            } else if (itemName1.isEmpty()&itemCode1.isPresent()&itemCode2.isEmpty()&itemCode3.isEmpty()) {
+                return new ResponseEntity(searchDataService.itemCode1SaveDataCSV(masterId, itemCode1, startTime, endTime).getBytes("CP949"), HttpStatus.OK);
+            } else if (itemName1.isEmpty()&itemCode1.isPresent()&itemCode2.isPresent()&itemCode3.isEmpty()){
+                return new ResponseEntity(searchDataService.itemCode2SaveDataCSV(masterId, itemCode1, itemCode2, startTime, endTime).getBytes("CP949"), HttpStatus.OK);
+            } else if (itemName1.isEmpty()&itemCode1.isPresent()&itemCode2.isPresent()&itemCode3.isPresent()){
+                return new ResponseEntity(searchDataService.itemCode3SaveDataCSV(masterId, itemCode1, itemCode2, itemCode3, startTime, endTime).getBytes("CP949"), HttpStatus.OK);
             }
         } catch (Exception e){
             e.printStackTrace();
--- src/main/java/com/wefunding/wdh/gs/ecos/service/SearchDataService.java
+++ src/main/java/com/wefunding/wdh/gs/ecos/service/SearchDataService.java
@@ -66,72 +66,72 @@ public class SearchDataService {
     }
 
 
-//    public String itemCode1SaveDataCSV(Optional<Integer> masterId, Optional<String> itemCode1, Optional<Integer> startTime, Optional<Integer> endTime) {
-//
-//        JSONArray jsonArr_result = new JSONArray();
-//
-//        List<SearchEntity> searchEntityList = searchEntityRepository.itemCode1Information(masterId,itemCode1, startTime, endTime);
-//        for (SearchEntity searchEntity : searchEntityList) {
-//            JSONObject result = new JSONObject();
-//            result.put("statName", searchEntity.getStatName());
-//            result.put("ItemCode1", searchEntity.getItemCode1());
-//            result.put("ItemName1", searchEntity.getItemName1());
-//            result.put("ItemCode2", searchEntity.getItemCode2());
-//            result.put("ItemName2", searchEntity.getItemName2());
-//            result.put("ItemCode3", searchEntity.getItemCode3());
-//            result.put("ItemName3", searchEntity.getItemName3());
-//            result.put("UnitName", searchEntity.getUnitName());
-//            result.put("Time", searchEntity.getTime());
-//            result.put("DataValue", searchEntity.getDataValue());
-//            jsonArr_result.put(result);
-//        }
-//        String csv = CDL.toString(jsonArr_result);
-//        return csv;
-//    }
-//    public String itemCode2SaveDataCSV(Optional<Integer> masterId, Optional<String> itemCode1, Optional<String> itemCode2, Optional<Integer> startTime, Optional<Integer> endTime) {
-//
-//        JSONArray jsonArr_result = new JSONArray();
-//
-//        List<SearchEntity> searchEntityList = searchEntityRepository.itemCode2Information(masterId, itemCode1, itemCode2,startTime, endTime);
-//        for (SearchEntity searchEntity : searchEntityList) {
-//            JSONObject result = new JSONObject();
-//            result.put("statName", searchEntity.getStatName());
-//            result.put("ItemCode1", searchEntity.getItemCode1());
-//            result.put("ItemName1", searchEntity.getItemName1());
-//            result.put("ItemCode2", searchEntity.getItemCode2());
-//            result.put("ItemName2", searchEntity.getItemName2());
-//            result.put("ItemCode3", searchEntity.getItemCode3());
-//            result.put("ItemName3", searchEntity.getItemName3());
-//            result.put("UnitName", searchEntity.getUnitName());
-//            result.put("Time", searchEntity.getTime());
-//            result.put("DataValue", searchEntity.getDataValue());
-//            jsonArr_result.put(result);
-//        }
-//        String csv = CDL.toString(jsonArr_result);
-//        return csv;
-//    }
-//    public String itemCode3SaveDataCSV(Optional<Integer> masterId, Optional<String> itemCode1, Optional<String> itemCode2, Optional<String> itemCode3, Optional<Integer> startTime, Optional<Integer> endTime) {
-//
-//        JSONArray jsonArr_result = new JSONArray();
-//
-//        List<SearchEntity> searchEntityList = searchEntityRepository.itemCode3Information(masterId, itemCode1, itemCode2, itemCode3,startTime, endTime);
-//        for (SearchEntity searchEntity : searchEntityList) {
-//            JSONObject result = new JSONObject();
-//            result.put("statName", searchEntity.getStatName());
-//            result.put("ItemCode1", searchEntity.getItemCode1());
-//            result.put("ItemName1", searchEntity.getItemName1());
-//            result.put("ItemCode2", searchEntity.getItemCode2());
-//            result.put("ItemName2", searchEntity.getItemName2());
-//            result.put("ItemCode3", searchEntity.getItemCode3());
-//            result.put("ItemName3", searchEntity.getItemName3());
-//            result.put("UnitName", searchEntity.getUnitName());
-//            result.put("Time", searchEntity.getTime());
-//            result.put("DataValue", searchEntity.getDataValue());
-//            jsonArr_result.put(result);
-//        }
-//        String csv = CDL.toString(jsonArr_result);
-//        return csv;
-//    }
+    public String itemCode1SaveDataCSV(Optional<Integer> masterId, Optional<String> itemCode1, Optional<Integer> startTime, Optional<Integer> endTime) {
+
+        JSONArray jsonArr_result = new JSONArray();
+
+        List<SearchEntity> searchEntityList = searchEntityRepository.itemCode1Information(masterId,itemCode1, startTime, endTime);
+        for (SearchEntity searchEntity : searchEntityList) {
+            JSONObject result = new JSONObject();
+            result.put("statName", searchEntity.getStatName());
+            result.put("ItemCode1", searchEntity.getItemCode1());
+            result.put("ItemName1", searchEntity.getItemName1());
+            result.put("ItemCode2", searchEntity.getItemCode2());
+            result.put("ItemName2", searchEntity.getItemName2());
+            result.put("ItemCode3", searchEntity.getItemCode3());
+            result.put("ItemName3", searchEntity.getItemName3());
+            result.put("UnitName", searchEntity.getUnitName());
+            result.put("Time", searchEntity.getTime());
+            result.put("DataValue", searchEntity.getDataValue());
+            jsonArr_result.put(result);
+        }
+        String csv = CDL.toString(jsonArr_result);
+        return csv;
+    }
+    public String itemCode2SaveDataCSV(Optional<Integer> masterId, Optional<String> itemCode1, Optional<String> itemCode2, Optional<Integer> startTime, Optional<Integer> endTime) {
+
+        JSONArray jsonArr_result = new JSONArray();
+
+        List<SearchEntity> searchEntityList = searchEntityRepository.itemCode2Information(masterId, itemCode1, itemCode2,startTime, endTime);
+        for (SearchEntity searchEntity : searchEntityList) {
+            JSONObject result = new JSONObject();
+            result.put("statName", searchEntity.getStatName());
+            result.put("ItemCode1", searchEntity.getItemCode1());
+            result.put("ItemName1", searchEntity.getItemName1());
+            result.put("ItemCode2", searchEntity.getItemCode2());
+            result.put("ItemName2", searchEntity.getItemName2());
+            result.put("ItemCode3", searchEntity.getItemCode3());
+            result.put("ItemName3", searchEntity.getItemName3());
+            result.put("UnitName", searchEntity.getUnitName());
+            result.put("Time", searchEntity.getTime());
+            result.put("DataValue", searchEntity.getDataValue());
+            jsonArr_result.put(result);
+        }
+        String csv = CDL.toString(jsonArr_result);
+        return csv;
+    }
+    public String itemCode3SaveDataCSV(Optional<Integer> masterId, Optional<String> itemCode1, Optional<String> itemCode2, Optional<String> itemCode3, Optional<Integer> startTime, Optional<Integer> endTime) {
+
+        JSONArray jsonArr_result = new JSONArray();
+
+        List<SearchEntity> searchEntityList = searchEntityRepository.itemCode3Information(masterId, itemCode1, itemCode2, itemCode3,startTime, endTime);
+        for (SearchEntity searchEntity : searchEntityList) {
+            JSONObject result = new JSONObject();
+            result.put("statName", searchEntity.getStatName());
+            result.put("ItemCode1", searchEntity.getItemCode1());
+            result.put("ItemName1", searchEntity.getItemName1());
+            result.put("ItemCode2", searchEntity.getItemCode2());
+            result.put("ItemName2", searchEntity.getItemName2());
+            result.put("ItemCode3", searchEntity.getItemCode3());
+            result.put("ItemName3", searchEntity.getItemName3());
+            result.put("UnitName", searchEntity.getUnitName());
+            result.put("Time", searchEntity.getTime());
+            result.put("DataValue", searchEntity.getDataValue());
+            jsonArr_result.put(result);
+        }
+        String csv = CDL.toString(jsonArr_result);
+        return csv;
+    }
 //
 //    public ResponseEntity searchTableSaveDataJSON(Optional<Integer> masterId, Optional<String> itemCode1, Optional<String> itemCode2, Optional<String> itemCode3, Optional<Integer> startTime, Optional<Integer> endTime) {
 //
